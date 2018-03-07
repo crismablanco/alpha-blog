dc69771442e58f4332a1b76b2b850b62d3b6f77c
Cristian Blanco <crismablanco@gmail.com>
2018-03-07 17:50:56 +0000

    Create and Update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,13 +6,32 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
-    @article.save!
     
-    redirect_to articles_show(@article)
+    if @article.save
+        flash[:notice] = "Article was successfully created"
+        redirect_to articles_path(@article)
+    else
+        render 'new'
+    end
   end
-  
+
   def show
     @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to articles_path(@article)
+    else
+        render 'edit'
+    end
   end
   
   def index
@@ -24,6 +43,5 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description)
     end
-  
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   
-  resource :articles
+  resources :articles
 end
