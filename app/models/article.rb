14a391a20a49b889947b09c9b40f850ff877c449
Cristian Blanco <crismablanco@gmail.com>
2018-03-14 17:48:40 +0000

    article meta details

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minumum: 3, maximum: 50}
   validates :description, presence: true, length: { minumum: 10, maximum: 1000 }
 end
